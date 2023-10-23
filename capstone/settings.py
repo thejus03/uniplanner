a3133a2e4c59519eadac077443792c84440f0766
thejus03 <142730611+thejus03@users.noreply.github.com>
2023-10-23 17:51:08 +0800

    Update settings.py

--- capstone/settings.py
+++ capstone/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = os.environ.get('KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 LOGIN_URL = "/login"
 
